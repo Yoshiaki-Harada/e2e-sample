a84e844be764ee6046054fdd4985adeb9722b48a
yoshiaki.harada <yoshiaki.harada@uzabase.com>
2021-06-06 11:03:59 +0900

    パイプラインの雛形を作成

---
+++ environments/pipeline/sns-api-pipeline.groovy
@@ -0,0 +1,26 @@
+pipeline{
+    agent any
+    options {
+        newContainerPerStage()
+        skipDefaultCheckout()
+        timestamp()
+    }
+    stages{
+        stage("A"){
+            steps{
+                echo "========executing A========"
+            }
+            post{
+                always{
+                    echo "========always========"
+                }
+                success{
+                    echo "========A executed successfully========"
+                }
+                failure{
+                    echo "========A execution failed========"
+                }
+            }
+        }
+    }
+}
