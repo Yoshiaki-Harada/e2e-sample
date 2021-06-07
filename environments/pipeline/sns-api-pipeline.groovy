c43b28f5e539e7af4666c3e492caaed716e6a3e0
yoshiaki.harada <yoshiaki.harada@uzabase.com>
2021-06-07 20:37:36 +0900

    カッコの修正

--- environments/pipeline/sns-api-pipeline.groovy
+++ environments/pipeline/sns-api-pipeline.groovy
@@ -46,9 +46,9 @@ pipeline {
                 stage('sequential') {
                     environment {
                         E2E_CONTEXT_HOST = sh(script: "kubectl --context minikube get nodes -o=jsonpath=\'{.items[0].status.addresses[0].address}\'", returnStdout: true)
-                        API_PORT=svcPort("minikube", "sns", "sns-api-svc")
+                        API_PORT=svcPort("minikube", "sns-e2e", "sns-api-svc")
                         API_URL="http://$E2E_CONTEXT_HOST:$API_PORT"
-                        DB_PORT=svcPort("minikube", "sns", "sns-db-svc")
+                        DB_PORT=svcPort("minikube", "sns-e2e", "sns-db-svc")
                         DB_URL="jdbc:postgresql://$E2E_CONTEXT_HOST:$DB_PORT/sns_db"
                         E2E_IMAGE="haradayoshiaki/gauge-gradle:latest"
                         E2E_ARGS="-e DB_URL=$DB_URL -u root -v /var/jenkins_home/workspace/sns-api-pipeline/e2e:/home/gradle/project -w /home/gradle/project"
@@ -86,7 +86,7 @@ pipeline {
                 //             sh """ gradle gauge -Ptags="!sequential" """
                 //         }
                 //     }
-                }
+                // }
             }
         }
     }
