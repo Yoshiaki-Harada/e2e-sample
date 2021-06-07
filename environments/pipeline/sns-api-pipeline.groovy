c503cc39ee18b13b109e5a9438b63aa0593372cc
yoshiaki.harada <yoshiaki.harada@uzabase.com>
2021-06-07 20:32:00 +0900

    sequentialで実行

--- environments/pipeline/sns-api-pipeline.groovy
+++ environments/pipeline/sns-api-pipeline.groovy
@@ -2,7 +2,6 @@ def svcPort(context, ns, svc) {
     return sh(script: "kubectl --context ${context} get svc ${svc} -n ${ns} -o=jsonpath=\'{.spec.ports[0].nodePort}\'", returnStdout: true)
 }
 
-def namespaces=["sns-e2e", "sns-e2e-seq"]
 pipeline {
     agent any
     options {
@@ -16,44 +15,40 @@ pipeline {
         }
         stage('deploy') {
             parallel {
-                namespaces.each {ns ->
-                stage('deploy api to $ns') {
+                stage('deploy api') {
                     steps {
                         dir('environments') {
                             script {
-                                sh """helm template --set namespace=$ns app/k8s/ | kubectl --context minikube apply -f -"""
+                                sh """helm template --set name=sns-e2e app/k8s/ | kubectl --context minikube apply -f -"""
                                 sh """kubectl wait --for=condition=ready pod -l name=sns-api -n sns --timeout=120s"""
                             }
                         }
                     }
                 }
-                stage('deploy db to $ns') {
+                stage('deploy db') {
                     steps {
                         dir('environments') {
                             script {
-                                sh """helm template --set namespace=$ns db/k8s/ | kubectl --context minikube apply -f -"""
+                                sh """helm template --set name=sns-e2e db/k8s/ | kubectl --context minikube apply -f -"""
                                 sh """kubectl wait --for=condition=ready pod -l name=sns-db -n sns --timeout=120s"""
                             }
                         }
                     }
-                }
                 }
             }
         }
         stage('e2e') {
             environment {
                 E2E_CONTEXT_HOST = sh(script: "kubectl --context minikube get nodes -o=jsonpath=\'{.items[0].status.addresses[0].address}\'", returnStdout: true)
+                API_PORT=svcPort("minikube", "sns", "sns-api-svc")
+                API_URL="http://$E2E_CONTEXT_HOST:$API_PORT"
+                DB_PORT=svcPort("minikube", "sns", "sns-db-svc")
+                DB_URL="jdbc:postgresql://$E2E_CONTEXT_HOST:$DB_PORT/sns_db"
                 E2E_IMAGE="haradayoshiaki/gauge-gradle:latest"
+                E2E_ARGS="-e DB_URL=jdbc:postgresql://192.168.39.122:30158/sns_db -u root -v /var/jenkins_home/workspace/sns-api-pipeline/e2e:/home/gradle/project -w /home/gradle/project"
             }
             parallel {
                 stage('sequential') {
-                    environment {
-                        API_PORT=svcPort("minikube", "sns-e2e", "sns-api-svc")
-                        API_URL="http://$E2E_CONTEXT_HOST:$API_PORT"
-                        DB_PORT=svcPort("minikube", "sns-e2e", "sns-db-svc")
-                        DB_URL="jdbc:postgresql://$E2E_CONTEXT_HOST:$DB_PORT/sns_db"
-                        E2E_ARGS="-e DB_URL=jdbc:postgresql://192.168.39.122:30158/sns_db -u root -v /var/jenkins_home/workspace/sns-api-pipeline/e2e:/home/gradle/project -w /home/gradle/project"
-                    }
                     agent {
                         docker {
                             image E2E_IMAGE
@@ -68,20 +63,6 @@ pipeline {
                     }
                 }
                 stage('parallel') {
-                    environment {
-                        API_PORT=svcPort("minikube", "sns-e2e-seq", "sns-api-svc")
-                        API_URL="http://$E2E_CONTEXT_HOST:$API_PORT"
-                        DB_PORT=svcPort("minikube", "sns-e2e-seq", "sns-db-svc")
-                        DB_URL="jdbc:postgresql://$E2E_CONTEXT_HOST:$DB_PORT/sns_db"
-                        E2E_ARGS="-e DB_URL=jdbc:postgresql://192.168.39.122:30158/sns_db -u root -v /var/jenkins_home/workspace/sns-api-pipeline/e2e:/home/gradle/project -w /home/gradle/project"
-                    }
-                    agent {
-                        docker {
-                            image E2E_IMAGE
-                            args E2E_ARGS
-                            reuseNode true
-                        }
-                    }
                     steps {
                         dir("e2e") {
                             sh """ gradle gauge -Ptags="!sequential" """
